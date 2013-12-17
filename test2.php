0f37be27013fc22b1602e17cde576c30b8d1733a
Shao <shaohan.chen@berkeley.edu>
2013-12-16 16:17:53 -0800

    [Added] applied style sheet to test2.php

--- test2.php
+++ test2.php
@@ -1,8 +1,40 @@
-<html>
+<!DOCTYPE html>
+
+<html class="full" lang="en">
+
 	<head>
-		<title>PHP TEST</title>
-	</head>
+    	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<meta name="description" content="">
+
+    	<title>Movie Trip - Search Result</title>
+
+	    <!-- CSS sheets -->
+	    <link href="css/style.css" rel="stylesheet">
+    	<link href="css/bootstrap.css" rel="stylesheet">
+	 </head>
+
 <body>
+
+<nav class="navbar navbar-fixed navbar-inverse">
+      <div class="container">
+          <div id="branding-logo">
+            <img src="image/icon.png" alt="logo">
+          </div><!-- #branding-logo -->
+        	<a class="navbar-brand" href="test1.html">Movie Trip</a>
+
+        <div id="menu">
+          <div class="collapse navbar-collapse navbar-ex1-collapse">
+            <ul class="nav navbar-nav">
+              <li><a href="#mytrips">My Trips</a></li>
+              <li><a href="#checkin-history">History</a></li>
+              <li><a href="#loginin" style=>Login</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.menu -->
+      </div><!-- /.container -->
+    </nav>
+
 
 <?php
 
@@ -19,7 +51,8 @@
 	$dec_obj=json_decode($SearchResult);                  // return parse result as an object
 	$dec_arr=json_decode($SearchResult, true);            // return parse result as an allay
 
-	echo "<p>Results:";
+	echo '<div class="container"><h3>Results:</h3>';
+
 	$a = 0;
 	while($a < count($dec_arr['title_popular']))
 		{
@@ -37,6 +70,8 @@
 			echo '<p><a href="test3.php?id=', $dec_arr['title_substring'][$b]['id'], '">', $dec_arr['title_substring'][$b]['title'], ' (', $desc, ')</a>';
         	$b++;
         }
+
+    echo '</div>';
 
 ?>
 </body>
